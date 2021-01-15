8a9d0083d1fdf434e91e43704ab0ad8e11d37549
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-15 20:55:18 +0500

    adding ban ners

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
         <h1 className="heading">EXPENSE TRACKER</h1>
+        <div className="conmtainer">
+          <div className="banner1">income</div>
+          <div className="banner2">total</div>
+          <div className="banner3">expenses</div>
+        </div>
      
     </div>
   );
