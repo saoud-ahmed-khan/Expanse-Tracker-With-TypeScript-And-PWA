b1f823998ef40c4ccf0777ca98d52e7fb099157f
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-21 01:21:47 +0500

    actions

--- src/components/Transection/Transection.tsx
+++ src/components/Transection/Transection.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { contaxt } from "../Context";
 export const Transection: React.FC = () => {
     const data = React.useContext(contaxt)
