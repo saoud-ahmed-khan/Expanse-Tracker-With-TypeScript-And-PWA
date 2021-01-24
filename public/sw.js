721c9cc7c15d9e80d053ace72001521fc233da1a
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-25 00:01:05 +0500

    pwa

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,3 @@
-console.log("sw from public")
+let cacheData="app1"
+this.addEventListener("install", (event)=>{event.waitUntil(caches.open(cacheData).then((cache)=>{cache.addAll(['/static/js/main.chunk.js','/static/js/0.chunk.js','/static/js/bundle.js','/index.html','/'])}))})
+this.addEventListener("fetch",(event)=>{event.respondWith(caches.match(event.request).then((resp)=>{if(resp){return resp}}))})
--- src/swDev.js
+++ src/swDev.js
@@ -4,3 +4,20 @@ export default function swDev()
     if('serviceWorker' in navigator)
     {navigator.serviceWorker.register(swURL).then((res)=>{console.log("response",res)})}
 }
+
+
+
+
+
+
+
+
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/8.2.4/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
