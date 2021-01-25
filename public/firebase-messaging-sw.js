9b76a4116776e43b67b847cf3cb25e038513d565
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-25 18:48:27 +0500

    adding sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,15 @@
 
-
+import firebase from "firebase/app";
+import "firebase/messaging";
+importScripts('https://www.gstatic.com/firebasejs/8.2.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.3/firebase-messaging.js');
 const firebaseConfig = {
-    apiKey: "AIzaSyA8-7y3o8AbXun5v2oVyjgSX49VCzkQuLs",
-    authDomain: "expensetrackerpwa-8105f.firebaseapp.com",
-    projectId: "expensetrackerpwa-8105f",
-    storageBucket: "expensetrackerpwa-8105f.appspot.com",
-    messagingSenderId: "776750821458",
-    appId: "1:776750821458:web:28f08e17e34dea34362f8b"
-  };
+  apiKey: "AIzaSyA8-7y3o8AbXun5v2oVyjgSX49VCzkQuLs",
+  authDomain: "expensetrackerpwa-8105f.firebaseapp.com",
+  projectId: "expensetrackerpwa-8105f",
+  storageBucket: "expensetrackerpwa-8105f.appspot.com",
+  messagingSenderId: "776750821458",
+  appId: "1:776750821458:web:28f08e17e34dea34362f8b"
+};
+firebaseConfig.initializeApp(firebaseConfig)
+firebase.messaging();
