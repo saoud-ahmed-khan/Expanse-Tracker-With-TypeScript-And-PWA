9a76cd8741a92f6719c26b361600a0815bc6f53c
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-01-25 17:49:46 +0500

    dding sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,2 +1,10 @@
 
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA8-7y3o8AbXun5v2oVyjgSX49VCzkQuLs",
+    authDomain: "expensetrackerpwa-8105f.firebaseapp.com",
+    projectId: "expensetrackerpwa-8105f",
+    storageBucket: "expensetrackerpwa-8105f.appspot.com",
+    messagingSenderId: "776750821458",
+    appId: "1:776750821458:web:28f08e17e34dea34362f8b"
+  };
